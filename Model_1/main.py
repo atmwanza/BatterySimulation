4b617d7b4fb073cfa011336538f505a70d62fe6f
Adam Mwanza <amwanza1@nait.ca>
2025-11-15 15:46:00 -0700

    code

--- Model_1/main.py
+++ Model_1/main.py
@@ -5,22 +5,34 @@ motor_voltage = 12 #volts
 lead_wc_eff = 0.358
 lead_bc_eff = 0.495
 ball_eff = 0.838
-nom_torq = 0.459
+nom_torq_ms = 0.459
+nom_torq_j = 50
+
 ang_velocity = [-10, -7.5, 2.5, 8, -5, -14, -16, -16, -14, -9, -1.25, 3.25, 7, 13, 14, 17, 17.5, 8, -2, -6.25 ]
 torques = [1,2,3,4]
 x_gait = []
-y_power = []
+y_power_ms = []
+y_power_j = []
 #functions
 def get_power_mech(torque,ang):
     return torque*ang
 
-def ang_vel_cycle():
+def p_motoroshaft_cycle():
     x_gait.clear()
-    y_power.clear()
+    y_power_ms.clear()
     for i in range (0,len(ang_velocity)):
         print(i)
         x_gait.append(i*5)
-        y_power.append(get_power_mech(nom_torq,ang_velocity[i-1]))
+        y_power_ms.append(get_power_mech(nom_torq_ms ,ang_velocity[i]))
+    return
+
+def p_joint_cycle():
+    x_gait.clear()
+    y_power_j.clear()
+    for i in range (0,len(ang_velocity)):
+        print(i)
+        x_gait.append(i*5)
+        y_power_j.append(get_power_mech(nom_torq_j ,ang_velocity[i]))
     return
 
 
@@ -34,16 +46,23 @@ def ang_vel_cycle():
 #x = [1, 2, 3, 4, 5]
 #y = [2, 5, 3, 7, 4]
 
+p_motoroshaft_cycle()
+p_joint_cycle()
 
-# plt.plot(x, y)
-ang_vel_cycle()
-#plt.plot(x_gait, ang_velocity)
-plt.plot(x_gait, y_power)
-
-
-plt.xlabel('Gait Cycle')
-plt.ylabel('Power Motorshaft')
+plt.figure(1)
+plt.plot(x_gait, y_power_ms)
+plt.axhline(y=0, color='gray', linestyle='-', linewidth=1)
+plt.axvline(x=0, color='gray', linestyle='-', linewidth=1)
+plt.xlabel('Gait Cycle %')
+plt.ylabel('Power Motorshaft W')
 plt.title('P_motorshaft vs time')
 
-# Display the plot
+plt.figure(2)
+plt.plot(x_gait,y_power_j)
+plt.axhline(y=0, color='gray', linestyle='-', linewidth=1)
+plt.axvline(x=0, color='gray', linestyle='-', linewidth=1)
+plt.xlabel('Gait Cycle %')
+plt.ylabel('Power Joint W')
+plt.title('P_joint vs time')
+
 plt.show()
