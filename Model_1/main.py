dcbf548e97541c20c1cbf415eddf2b4ce257cdef
Adam Mwanza <amwanza1@nait.ca>
2025-11-15 15:21:42 -0700

    p_mshaft created

--- Model_1/main.py
+++ Model_1/main.py
@@ -5,7 +5,8 @@ motor_voltage = 12 #volts
 lead_wc_eff = 0.358
 lead_bc_eff = 0.495
 ball_eff = 0.838
-angs = [1,4,9,16]
+nom_torq = 0.459
+ang_velocity = [-10, -7.5, 2.5, 8, -5, -14, -16, -16, -14, -9, -1.25, 3.25, 7, 13, 14, 17, 17.5, 8, -2, -6.25 ]
 torques = [1,2,3,4]
 x_gait = []
 y_power = []
@@ -13,13 +14,14 @@ y_power = []
 def get_power_mech(torque,ang):
     return torque*ang
 
-def cycle():
+def ang_vel_cycle():
     x_gait.clear()
     y_power.clear()
-    for i in range (1,len(torques)+1):
+    for i in range (0,len(ang_velocity)):
+        print(i)
         x_gait.append(i*5)
-        y_power.append(get_power_mech(torques[i-1],angs[i-1]))
-
+        y_power.append(get_power_mech(nom_torq,ang_velocity[i-1]))
+    return
 
 
 #ignore thermal limits
@@ -29,11 +31,13 @@ def cycle():
 
 
 # just to test matplotlib works on vscode
-x = [1, 2, 3, 4, 5]
-y = [2, 5, 3, 7, 4]
+#x = [1, 2, 3, 4, 5]
+#y = [2, 5, 3, 7, 4]
 
 
 # plt.plot(x, y)
+ang_vel_cycle()
+#plt.plot(x_gait, ang_velocity)
 plt.plot(x_gait, y_power)
 
 
