92e505c9917dc2720eb558330d8a42f44521ea70
siwira <144753952+siwira@users.noreply.github.com>
2025-11-21 20:25:45 -0700

    Update data source and adjust current calculation

--- GaitPlotPractice.py
+++ GaitPlotPractice.py
@@ -6,8 +6,9 @@ import math
 time_data = []
 joint_power_data = []
 
+
 #grab data from this file and make two lists
-with open('kneeJointPower.csv', 'r', newline='') as power_file:
+with open('kneeJointPower_final.csv', 'r', newline='') as power_file:
     file_reader = csv.reader(power_file)
     header = next(file_reader)
     time_col_idx = 0
@@ -16,15 +17,16 @@ with open('kneeJointPower.csv', 'r', newline='') as power_file:
         if len(row) > power_col_idx:
             time_data.append(float(row[time_col_idx]))
             joint_power_data.append(float(row[power_col_idx]))
-print(len(time_data))
-print(len(joint_power_data))
+# print(len(time_data))
+# print(len(joint_power_data))
+
 #Define some global constants
 motor_voltage = 12.0 #volts
 lead_wc_eff = 0.358
 lead_bc_eff = 0.495
 ball_eff = 0.838
 assumed_weight = 80 #kg
-R_phase = 0.06882 # 
+R_phase = 0.06882 # Ohms
 
 #ignore thermal limits
 #inputs: torque, angular velocity
@@ -36,18 +38,19 @@ joint_power_data_assuming_weight = [x * assumed_weight for x in joint_power_data
 motor_power_data = [ x * ball_eff for x in joint_power_data_assuming_weight]
 #assume P_motor == P_elec
 # P_motor = V * I(t) + I^2(t)*R
-#find the current (assumed to take the "plus" solution to the quadratic equation), gotta double check the current being negative.
-current_data = [ -(-motor_voltage + math.sqrt(motor_voltage**2 + 4 * R_phase * p_motor))/(2*R_phase) for p_motor in motor_power_data]
+#find the current (assumed to take the "plus" solution to the quadratic equation)
+current_data = [ (-motor_voltage + math.sqrt(motor_voltage**2 + 4 * R_phase * p_motor))/(2*R_phase) for p_motor in motor_power_data]
 
 # plt.plot(time_data, joint_power_data_assuming_weight)
 
 plt.plot(time_data, current_data)
-
+# plt.plot()
+plt.axhline(y=0, color="grey", linestyle = '-', linewidth='1')
 # plt.xlabel('Time (sec)')
 # plt.ylabel('Power per kg (W/kg)')
 # plt.title('Knee Joint Mechanical Power')
-plt.xlabel('Time (sec)')
-plt.ylabel('Current (A)')
-plt.title('Current output in the Gait Cycle')
+# plt.xlabel('Time (sec)')
+# plt.ylabel('Current (A)')
+# plt.title('Current output in the Gait Cycle')
 # Display the plot
 plt.show()
